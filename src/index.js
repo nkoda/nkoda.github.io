2ae2fd330bda7011b0810d7630c0db9c0c625b99
Nikko <dumriquenikko@gmail.com>
2022-12-29 13:47:18 -0800

    linked google analytics to the index.js file

--- src/index.js
+++ src/index.js
@@ -4,9 +4,30 @@ import './index.css'
 import App from './App.js'
 import { BrowserRouter } from 'react-router-dom'
 
+const injectGA = () => {
+    if (typeof window == 'undefined') {
+      return;
+    }
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+  
+    gtag('config', 'G-6H1G1NCXX8');
+  };
+
 ReactDOM.render(
+    
     <BrowserRouter>
         <App />
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-6H1G1NCXX8"
+        />
+    <script>{injectGA()}</script>
     </BrowserRouter>,
     document.getElementById('root')
 )
+
